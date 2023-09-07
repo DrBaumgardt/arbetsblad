0e628998a4d14677d44babd72e047e1bf1b96e1c
DrBaumgardt <baumgardt@gmail.com>
2023-09-07 08:31:21 +0200

    Innan ny struktur med slump av frågor

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "katex/dist/katex.min.css"; // Import KaTeX styles
+import { InlineMath, BlockMath } from "react-katex";
 
 interface TaskCardProps {
   task: any;
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
+  // Split the string based on $...$ and $$...$$
+  const parts = task.fråga.split(/((?:\${1,2})[^\$]*(?:\${1,2}))/);
+
   return (
     <div
       className="task-card"
@@ -21,8 +26,21 @@ const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
         </strong>
       </p>
       <div>
-        {/* Din logik för att visa uppgiften, inklusive eventuella LaTeX-formler */}
-        {task.fråga}
+        {/* Loop through the parts and render either text or LaTeX */}
+        {parts.map((part: string, index: number) => {
+          if (part.startsWith("$$") && part.endsWith("$$")) {
+            // This is a block LaTeX part
+            const latex = part.substring(2, part.length - 2);
+            return <BlockMath key={index} math={latex} />;
+          } else if (part.startsWith("$") && part.endsWith("$")) {
+            // This is an inline LaTeX part
+            const latex = part.substring(1, part.length - 1);
+            return <InlineMath key={index} math={latex} />;
+          } else {
+            // This is a text part
+            return part;
+          }
+        })}
       </div>
     </div>
   );
